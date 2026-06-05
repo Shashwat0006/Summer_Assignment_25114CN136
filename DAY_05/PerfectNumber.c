849f6ff8b47d4e52d6846b4b109b40bb46dd690d
Shashwat0006 <25114CN136@glbitm.ac.in>
2026-06-05 09:10:32 +0530

    Update PerfectNumber.c

--- DAY_05/PerfectNumber.c
+++ DAY_05/PerfectNumber.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int num, temp, digit;
-    int sum = 0, fact, i;
+    int num, i, sum = 0;
 
-    // Input number
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    temp = num;
+    /*
+        Logic:
+        1. Find all proper divisors of the number.
+        2. Add those divisors.
+        3. If sum of divisors equals the number,
+           then it is a Perfect Number.
+    */
 
-    // Calculate sum of factorials of digits
-    while(temp > 0) {
-        digit = temp % 10;
-
-        fact = 1;
-        for(i = 1; i <= digit; i++) {
-            fact *= i;
+    for(i = 1; i < num; i++) {
+        if(num % i == 0) {
+            sum += i;
         }
-
-        sum += fact;
-        temp /= 10;
     }
 
-    // Check strong number
-    if(sum == num) {
-        printf("%d is a Strong Number.\n", num);
-    } else {
-        printf("%d is not a Strong Number.\n", num);
-    }
+    if(sum == num)
+        printf("%d is a Perfect Number.", num);
+    else
+        printf("%d is not a Perfect Number.", num);
 
     return 0;
 }
