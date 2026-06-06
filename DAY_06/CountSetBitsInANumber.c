888a06907b05fd356e4152bdfc26fba0f107d09a
Shashwat0006 <25114CN136@glbitm.ac.in>
2026-06-06 15:38:34 +0530

    Update CountSetBitsInANumber.c

--- DAY_06/CountSetBitsInANumber.c
+++ DAY_06/CountSetBitsInANumber.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int num, count = 0;
+int main () 
+{
+int num , count = 0;
 
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    // Logic:
-    // Check the last bit using num % 2.
-    // If it is 1, increment count.
-    // Right shift the number by dividing by 2.
+   printf("enter a number : ");
+    scan("%d", &num);
+    /*Logic:
+     Check the last bit using num % 2.
+    If it is 1, increment count.
+    */ Right shift the number by dividing by 2.
 
     while (num > 0) {
         if (num % 2 == 1) {
             count++;
         }
-        num = num / 2;
+       num = num/2;
     }
 
     printf("Number of set bits = %d\n", count);
