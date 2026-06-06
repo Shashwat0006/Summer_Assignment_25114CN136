9f8e252f89a359923d1683e25108a3f794f8dc9d
Shashwat0006 <25114CN136@glbitm.ac.in>
2026-06-06 16:01:52 +0530

    Update ConvertDecimalToBinary.c

--- DAY_06/ConvertDecimalToBinary.c
+++ DAY_06/ConvertDecimalToBinary.c
@@ -2,19 +2,20 @@
 int main()
 {
   
-int decimal , binary[64] , I=0
+int decimal , binary[64] , i=0;
 
    printf("enter a decimal number :");
     scanf("%d", &decimal);
 
     /* Logic:
    Repeatedly divide the number by 2.
-     Store the remainder (0 or 1).
+     Store the remainder (0 or 1).23
      Remainders collected in reverse order form the binary number.
 */
 
     while (decimal > 0) {
-        binary[i] = decimal % 2;
+
+        binary [i] = decimal % 2;
         decimal = decimal / 2;
         i++;
     }
@@ -23,7 +24,7 @@ int decimal , binary[64] , I=0
 
     // Print binary digits in reverse order
     for (int j = i - 1; j >= 0; j--) {
-       printf("%d",binary[j];
+       printf("%d",binary[j]);
     }
 
     printf("\n");
