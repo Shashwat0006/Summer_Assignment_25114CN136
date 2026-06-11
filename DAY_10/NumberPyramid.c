7028e868211a33133827cd5e121e1b29daa55e29
Shashwat0006 <25114CN136@glbitm.ac.in>
2026-06-11 21:09:48 +0530

    Update NumberPyramid.c

--- DAY_10/NumberPyramid.c
+++ DAY_10/NumberPyramid.c
@@ -5,7 +5,7 @@ int main()
     int i,j,n =5;
 
     // Outer loop controls rows
-    for(i = 1; i <= n; i++)
+      for(i = 1 ; i <=n; i++)
     {
         // Print leading spaces
         for(j = 1; j <= n - i; j++)
@@ -17,14 +17,14 @@ int main()
         // EXAMPLE=23
         for(j = 1; j <= i; j++)
         {
-            printf("%d", j);
+           printf("%d",j);
         }
 
         // Print descending numbers
         //EXAMPLE=23
         for(j = i - 1; j >= 1;j--)
         {
-            printf("%d", j);
+            printf("%d",j);
         }
 
         printf("\n");
